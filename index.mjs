3f83fc642d36dd043ba99f699357b8020e0000ad
Shigue Ogino <shigue.ogino@usach.cl>
2023-11-08 18:52:58 -0300

    Agrega niveles de dificultad basados en la edad del jugador

--- index.mjs
+++ index.mjs
@@ -3,7 +3,21 @@ import { getRandomNumber, run } from "./support.mjs";
 console.log('🚀');
 
 run(async (question) => {
-    const name = await question('Enter your name: ');
-    const lucky_number = getRandomNumber(1, 7);
-    console.log(`Hello ${name}! Your lucky number is ${lucky_number}.`);
+    const secretNumber = getRandomNumber(1, 100);
+    console.log('Try to guess the secret number between 1 and 100. You have 3 attempts.');
+
+    for (let i = 0; i < 3; i++) {
+        const guess = parseInt(await question('Enter your guess: '), 10);
+
+        if (guess === secretNumber) {
+            console.log(`Congratulations ${name}! You've guessed the correct number.`);
+            return;
+        } else if (guess < secretNumber) {
+            console.log('Your guess is too low.');
+        } else {
+            console.log('Your guess is too high.');
+        }
+    }
+
+    console.log(`Game over! The secret number was ${secretNumber}.`);
 });
